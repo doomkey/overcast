b74e128eec514b98c4887a6f34150feb5a7f6cb3
Imran Molla Joy <imranmollajoy@gmail.com>
2026-03-09 14:25:47 +0600

    random stuff

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script lang="ts">
 	import './layout.css';
 	// import favicon from '$lib/assets/favicon.svg';
+	import banner from '$lib/assets/banner_overcast.png';
 
 	let { children } = $props();
 </script>
 
+<svelte:head>
+	<meta property="og:image" content={banner} />
+</svelte:head>
 <!-- <svelte:head><link rel="icon" href={favicon} /></svelte:head> -->
 {@render children()}
